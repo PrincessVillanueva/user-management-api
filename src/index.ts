e5062915c48b9fe8234287c737a97bc7ab2e1515
Mark Enfermo <112786278+orenji-neko@users.noreply.github.com>
2025-03-29 14:52:05 +0800

    Added routes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
+import Express from "express";
+import type { Request, Response } from "express";
 
-console.log("hello world");
+const app = Express();
+
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+/**
+ * [GET] /users/
+ * @author INSERT NAME HERE
+ */
+app.get("/users", (req: Request, res: Response) => {
+
+});
+
+/**
+ * [POST] /users/
+ * @author INSERT NAME HERE
+ */
+app.post("/users", (req: Request, res: Response) => {
+
+});
+
+/**
+ * [DELETE] /users/:id
+ * @author INSERT NAME HERE
+ */
+app.delete("/users/:id", (req: Request, res: Response) => {
+  
+});
+
+app.listen(3000, (err) => {
+  console.log("Server running...");
+});
